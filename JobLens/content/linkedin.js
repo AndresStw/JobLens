f67064ae77c84b2f077668c2cf840b16150bc162
kevin L <cadl0898@gmail.com>
2026-06-27 18:23:23 -0500

    linkedin con extraccion de datos

--- JobLens/content/linkedin.js
+++ JobLens/content/linkedin.js
@@ -1,1 +1,45 @@
-console.log("JobLens activo en LinkedIn");
+(() => {
+  console.log("JobLens activo en LinkedIn");
+
+  const isJobPage = window.location.pathname.includes("/jobs/view/");
+
+  if (!isJobPage) return;
+
+  const offer = {
+    title: "",
+    company: "",
+    location: "",
+    description: ""
+  };
+
+  function extractJob() {
+    offer.title =
+      document
+        .querySelector(".job-details-jobs-unified-top-card__job-title")
+        ?.innerText.trim() || "";
+
+    offer.company =
+      document
+        .querySelector(".job-details-jobs-unified-top-card__company-name")
+        ?.innerText.trim() || "";
+
+    offer.location =
+      document
+        .querySelector(
+          ".job-details-jobs-unified-top-card_primary-description-container",
+        )
+        ?.innerText.trim() || "";
+
+    offer.description =
+      document.querySelector(".jobs-descriptions__content")?.innerText.trim() ||
+      "";
+
+    chrome.storage.local.set({
+      currentOffer: offer,
+    });
+
+    console.log(offer);
+  }
+
+  setTimeout(extractJob, 2000);
+})();
